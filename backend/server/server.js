5bf8c358be4b0db70685d4edf405fb6ebde1b96d
hyunho4532 <kyounguk192@gmail.com>
2024-03-25 00:13:09 +0900

    2024-03-25 backend Platform JSON 데이터 가져오기

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const https = require('https');
+
 const app = express();
 
 app.listen(50000, (response) => {
-    console.log(response);
+    https.get('https://czwpwf5o4m.execute-api.ap-northeast-2.amazonaws.com/stage/error/write', (response) => {
+        let data = '';
+
+        response.on('data', (chunk) => {
+            data += chunk;
+
+            console.log(data);
+        });
+    }).on('error', (error) => {
+        console.error(error);
+    })
 })
