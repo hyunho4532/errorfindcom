60b59464c90166fd6054f0d838f8d1ba4bdb3075
hyunho4532 <kyounguk192@gmail.com>
2024-03-25 23:23:58 +0900

    2024-03-25 에러 작성 글 구현 중 (HTTP API - Platform 파싱)

--- frontend/src/view/write/errorWriteBoard.tsx
+++ frontend/src/view/write/errorWriteBoard.tsx
@@ -5,13 +5,22 @@ import axios from "axios";
 function errorWriteBoard() {
 
     const [errorTypeData, setErrorTypeData] = useState([]);
+    const [platformData, setPlatformData] = useState([]);
 
     useEffect(() => {
-        axios.post("http://localhost:50000/fetchData")
+        axios.post("http://localhost:50000/errorTypeData")
             .then(response => {
 
                 setErrorTypeData(response.data);
 
+            })
+            .catch(error => {
+                console.error(error);
+            })
+
+        axios.post("http://localhost:50000/platformData")
+            .then(response => {
+                setPlatformData(response.data);
             })
             .catch(error => {
                 console.error(error);
@@ -25,14 +34,27 @@ function errorWriteBoard() {
                 <h2 className="error-write-board-component-where-title">1. 에러가 어디에서 발생했나요? 🤔🤔</h2>
                 <div className="selectBox">
                     <select name="fruits" className="select">
-                        { errorTypeData.map((value: any) => <option>{value}</option>) }
+                        { platformData.map((value: any) => <option>{value}</option>) }
                     </select>
-                    <span className="icoArrow"></span>
                 </div>
             </div>
 
             <div className="error-write-board-component-second-group">
-                <h2 className="error-write-board-component-kind">2. 에러 종류를 선택해주세요! 🤔🤔</h2>
+                <h2 className="error-write-board-component-kind">2. 에러 종류를 선택해주세요 😏😏</h2>
+                <div className="selectBox">
+                    <select name="fruits" className="select">
+                        { errorTypeData.map((value: any) => <option>{value}</option> )}
+                    </select>
+                </div>
+            </div>
+
+            <div className="error-write-board-component-second-group">
+                <h2 className="error-write-board-component-kind">3. 간략하게 에러 내용을 입력해주세요 😌😌</h2>
+                <div className="selectBox">
+                    <select name="fruits" className="select">
+                        { errorTypeData.map((value: any) => <option>{value}</option> )}
+                    </select>
+                </div>
             </div>
         </>
     )
